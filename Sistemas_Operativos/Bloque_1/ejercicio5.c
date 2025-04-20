48d98b77ef126f469c9fe506409a5e10afe2fefd
Jaime Hernández Pérez <i32hepej@uco.es>
2025-04-20 11:16:12 +0200

    Update ejercicio5.c
    Ejercicio 5 hecho

--- Sistemas_Operativos/Bloque_1/ejercicio5.c
+++ Sistemas_Operativos/Bloque_1/ejercicio5.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		printf("Uso: %s <número> <número>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	pid_t pid;
+	pid = fork();
+	if (pid == 0) {
+		execlp("./factorial", "./factorial", argv[1], NULL);
+		printf("execlp ha dado error.");
+		exit(EXIT_FAILURE);
+	}
+	pid = fork();
+	if (pid == 0) {
+		execlp("./factorial", "./factorial", argv[2], NULL);
+		printf("execlp ha dado error.");
+		exit(EXIT_FAILURE);
+	}
+}
