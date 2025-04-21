539ca8ad5eedb0cb86fb3376bd8eacd6e8c1b7d0
Jaime Hernández Pérez <i32hepej@uco.es>
2025-04-21 10:51:54 +0200

    Update ejercicio6.c
    Ejercicio 6 hecho

--- Sistemas_Operativos/Bloque_1/ejercicio6.c
+++ Sistemas_Operativos/Bloque_1/ejercicio6.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
 
+static int count=0;
+	
+void controlador(int sig) {
+	if (count == 0) {
+		alarm(3);
+		printf("RING\n");
+	}
+	if (count == 4) {
+		kill(getpid(), SIGTERM);
+	}
+	if (count != 0) {
+		alarm(1);
+		printf("RING\n");
+	}
+	count++;
+}
+
+int main() {
+	alarm(5); //Primera alarma en 5 segundos
+	signal(SIGALRM, controlador);
+	while (1) {
+//Bucle infinito (aunque se detendrá con la señal SIGTERM)
+	}
+	return 0;
+}
